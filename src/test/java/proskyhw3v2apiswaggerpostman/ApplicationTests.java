fde3c400e7b759e039ea8b5c0a020c3a3462dfcc
Танин <mazayztn@yandex.ru>
2024-03-16 11:32:07 +0300

    initial commit

---
+++ src/test/java/proskyhw3v2apiswaggerpostman/ApplicationTests.java
@@ -0,0 +1,13 @@
+package proskyhw3v2apiswaggerpostman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
